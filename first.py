505c47b52ba70cfa094db4e0bd97f303e23b267b
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-12 09:49:51 +0530

    commit

---
+++ first.py
@@ -0,0 +1,17 @@
+import os
+import openai
+
+os.environ["OPENAI_API_KEY"] = "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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=" Q: How does a telescope work \nA:",
+  temperature=0,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  stop=["\n"]
+)
+print(response)
